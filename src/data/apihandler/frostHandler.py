ab2253dfbbbcc300cbc57f6cbea47ef412351399
h593286 <593286@stud.hvl.no>
2024-02-20 11:19:14 +0100

    Additions to poetry + frosthandler
    will add tests

---
+++ src/data/apihandler/frostHandler.py
@@ -0,0 +1,45 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class FrostHandler:
+    def __init__(self):
+        self.FROST_CLIENT_ID = os.getenv('FROST_CLIENT_ID')
+        self.FROST_CLIENT_SECRET = os.getenv('FROST_CLIENT_SECRET')
+        self.observationEndpoint = 'https://frost.met.no/observations/v0.jsonld'
+        self.sourcesEndpoint = 'https://frost.met.no/sources/v0.jsonld'
+
+    def sendObservationRequest(self, source, time='latest'):
+        parameters = {
+            'sources': source,
+            'referencetime': time,
+            'elements': 'air_temperature,relative_humidity,wind_speed'}
+
+        response = requests.get(self.observationEndpoint,
+                                params=parameters,
+                                auth=(self.FROST_CLIENT_ID, self.FROST_CLIENT_SECRET))
+
+        return response.json()
+
+    def nearestStation(self, long, lat):
+
+        parameters = {
+            'types': 'SensorSystem',
+            'elements': 'air_temperature,relative_humidity,wind_speed',
+            'geometry': f'nearest(POINT({long} {lat}))'}
+
+        response = requests.get(self.sourcesEndpoint,
+                                params=parameters,
+                                auth=(self.FROST_CLIENT_ID, self.FROST_CLIENT_SECRET))
+
+        json = response.json()
+        stationId = json['data'][0]['id']
+        return stationId
+
+
+frostHandler = FrostHandler()
+nearest = frostHandler.nearestStation(10,59)
+
+r = frostHandler.sendObservationRequest(nearest)
+print(r)

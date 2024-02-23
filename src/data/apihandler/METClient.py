9950fcf03b6306dc3451ec65c6490b83f45a8fd3
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2024-02-23 16:54:25 +0100

    request call to MET forcast given long/lat

--- src/data/apihandler/METClient.py
+++ src/data/apihandler/METClient.py
@@ -1,9 +1,30 @@
+import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 # METclient, simple contact point for MET(forecast)
 # Only gets the observation, does not format it
 class METClient:
     def __init__(self):
-        self.client = None
+        self.FROST_CLIENT_ID = os.getenv('FROST_CLIENT_ID')
+        self.FROST_CLIENT_SECRET = os.getenv('FROST_CLIENT_SECRET')
+        self.forecastEndpoint = 'https://api.met.no/weatherapi/locationforecast/2.0/compact'
 
+    def sendForecastRequest(self,  long, lat):
 
+        parameters = {
+            'lat': lat,
+            'lon': long}
+
+        #User-Agent needs to be changed, it requires the user agent not to be a python script, acts as an identifire(needs to be unique)
+        headers = {
+            'User-Agent': 'Test'
+        }
+        #auth is not used
+        response = requests.get(self.forecastEndpoint,
+                                params=parameters,
+                                headers=headers,
+                                auth=(self.FROST_CLIENT_ID, self.FROST_CLIENT_SECRET))
+        return response

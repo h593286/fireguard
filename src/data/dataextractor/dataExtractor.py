5910a28716cb22780bd44f7cff760c34f8807e9b
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2024-02-23 13:15:03 +0100

    extracts temperature and humidity from a location given long and lat

--- src/data/dataextractor/dataExtractor.py
+++ src/data/dataextractor/dataExtractor.py
@@ -1,18 +1,22 @@
+from src.data.apihandler.apihandler import FrostClient
 
 
 # Extracts the wanted data from the observations and forecasts.
 # Same format for storage
 class DataExtractor:
+    def __init__(self):
+        self.FrostClient = FrostClient()
 
-    def extractObservation(self, observation):
-
-        # ToDo
+    def extractObservation(self, observation, long, lat):
         # Take out the nessecary elements from the observation and return it
-
-        return False
+        #not sure what "observations" was supposed to do
+        response = FrostClient().sendObservationRequest(long, lat)
+        data = response.json()['data'][0]
+        observations = data['observations']
+        returnJson = {'sourceId': data['sourceId'], 'referenceTime': data['referenceTime'], 'air_temperature': observations[0]['value'], 'relative_humidity': observations[1]['value']}
+        return returnJson
 
     def extractForecast(self, forecast):
-
         # ToDo
         # Take out the nessecary elements from the forecast and return it
 

b5adefd77462206511399a90656fb6b0890e4f58
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2024-02-24 09:49:04 +0100

    extractForecast

--- src/data/dataextractor/dataExtractor.py
+++ src/data/dataextractor/dataExtractor.py
@@ -1,11 +1,13 @@
 from src.data.apihandler.apihandler import FrostClient
-
+#Check if correct import
+from src.data.apihandler.METClient import METClient
 
 # Extracts the wanted data from the observations and forecasts.
 # Same format for storage
 class DataExtractor:
     def __init__(self):
         self.FrostClient = FrostClient()
+        self.METClient = METClient()
 
     def extractObservation(self, observation, long, lat):
         # Take out the nessecary elements from the observation and return it
@@ -16,8 +18,9 @@ class DataExtractor:
         returnJson = {'sourceId': data['sourceId'], 'referenceTime': data['referenceTime'], 'air_temperature': observations[0]['value'], 'relative_humidity': observations[1]['value']}
         return returnJson
 
-    def extractForecast(self, forecast):
-        # ToDo
+    def extractForecast(self, forecast, long, lat):
         # Take out the nessecary elements from the forecast and return it
+        response = METClient().sendForecastRequest(long, lat)
+        data = response.json()
 
-        return False
+        return data

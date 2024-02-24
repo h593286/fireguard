ae2eec663fff3d048ea69c8916152e0a817caf2a
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2024-02-24 12:36:07 +0100

    Added datatypes from original project, fixed dataextractor from observations

---
+++ src/data/dataTypes.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+import datetime
+
+class Location(BaseModel):
+
+    latitude: float
+    longitude: float
+class WeatherDataPoint(BaseModel):
+
+    temperature: float
+    humidity: float
+    wind_speed: float
+    timestamp: datetime.datetime
+
+    def __str__(self):
+
+        format_str = f'WeatherData[{self.timestamp}] {self.temperature, self.humidity, self.wind_speed}]'
+
+        return format_str
+
+
+class Observations(BaseModel):
+
+    source: str
+    location: Location
+    data: list[WeatherDataPoint]
+
+    def __str__(self):
+        format_str = f'Observations [Source: {self.source} @ Location: {self.location}]\n'
+
+        # Join all data points using '\n' as a separator
+        data_strings = '\n'.join(map(str, self.data))
+
+        return format_str + data_strings + '\n'
--- src/data/dataextractor/dataExtractor.py
+++ src/data/dataextractor/dataExtractor.py
@@ -1,4 +1,9 @@
 from src.data.apihandler.apihandler import FrostClient
+from src.data.dataTypes import *
+import numpy as np
+import dateutil.parser
+
+
 #Check if correct import
 from src.data.apihandler.METClient import METClient
 
@@ -9,15 +14,55 @@ class DataExtractor:
         self.FrostClient = FrostClient()
         self.METClient = METClient()
 
-    def extractObservation(self, observation, long, lat):
+    def extractObservation(self, location: Location):
         # Take out the nessecary elements from the observation and return it
-        #not sure what "observations" was supposed to do
-        response = FrostClient().sendObservationRequest(long, lat)
-        data = response.json()['data'][0]
-        observations = data['observations']
-        returnJson = {'sourceId': data['sourceId'], 'referenceTime': data['referenceTime'], 'air_temperature': observations[0]['value'], 'relative_humidity': observations[1]['value']}
-        return returnJson
 
+        response = FrostClient().sendObservationRequest(location)
+
+        data_list = response.json()['data']
+
+        weatherdatapoints = list()
+
+        source_id = None
+
+        if len(data_list) >= 1:
+
+            source_id = data_list[0]['sourceId']
+
+            for data in data_list:
+
+                reference_time = dateutil.parser.parse(data['referenceTime'])
+                station_observations = data['observations']
+
+                temperature = np.nan
+                relative_humidity = np.nan
+                wind_speed = np.nan
+
+                for station_observation in station_observations:
+
+                    # string to datatime object required
+                    timestamp = reference_time  # assume that observations have the same time stamp
+
+                    # TODO: rewrite to use a switch
+                    if station_observation['elementId'] == 'air_temperature':
+                        temperature = station_observation['value']
+                    elif station_observation['elementId'] == 'relative_humidity':
+                        relative_humidity = station_observation['value']
+                    elif station_observation['elementId'] == 'wind_speed':
+                        wind_speed = station_observation['value']
+
+                wd_point = WeatherDataPoint(temperature=temperature,
+                                            humidity=relative_humidity,
+                                            wind_speed=wind_speed,
+                                            timestamp=timestamp
+                                            )
+
+                weatherdatapoints.append(wd_point)
+
+        # TODO: maybe also source as part of the parameters - or extract weather data function instead
+        observations = Observations(source=source_id, location=location, data=weatherdatapoints)
+        print(observations)
+        return observations
     def extractForecast(self, forecast, long, lat):
         # Take out the nessecary elements from the forecast and return it
         response = METClient().sendForecastRequest(long, lat)

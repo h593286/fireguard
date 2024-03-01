d91d3fac97d2703a2cc260acef0e882610f47944
h593286 <593286@stud.hvl.no>
2024-03-01 11:54:39 +0100

    Updates

--- src/client/clientprototype.py
+++ src/client/clientprototype.py
@@ -1,39 +1,32 @@
 import json
 import requests
-from flask import Flask, request, send_from_directory
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-# @app.route('/', methods=['GET'])
-# def form_display():
-#     with open('cities.json', 'r') as json_file:
-#         data = json.load(json_file)
+@app.route('/', methods=['GET'])
+def form_display():
+    with open('cities.json', 'r') as json_file:
+        data = json.load(json_file)
 
-#     dropdown_html = '<select name="city">'
-#     for city_info in data:
-#         city_name = city_info['city']
-#         dropdown_html += f'<option value="{city_name}">{city_name}</option>'
-#     dropdown_html += '</select>'
+    dropdown_html = '<select name="city">'
+    for city_info in data:
+        city_name = city_info['city']
+        dropdown_html += f'<option value="{city_name}">{city_name}</option>'
+    dropdown_html += '</select>'
 
-#     return f'''
-#         <form action="http://127.0.0.1:5001/get_firerisk_city" method="get">
-#             {dropdown_html}
-#             <input type="submit" value="Get TTF"/>
-#         </form>
-#         <form action="http://127.0.0.1:5001/fire_risk" method="post"> 
-#             <input type="text" name="lat" placeholder="Latitude"/>
-#             <input type="text" name="lng" placeholder="Longitude"/>
-#             <input type="submit" value="Get TTF"/>
-#         </form>
-
-
-#     ''' ##MÅ OPPDATERE ENDPOINT NÅR VI HAR FÅTT API
-
-@app.route("/<path:path>")
-def files(path: str):
-    print(path)
-    return send_from_directory("../../static",path)
+    return f'''
+        <form action="http://127.0.0.1:5001/get_firerisk_city" method="get">
+            {dropdown_html}
+            <input type="submit" value="Get TTF"/>
+        </form>
+        <form action="http://127.0.0.1:5001/get_firerisk_coordinates" method="get"> 
+            <input type="text" name="lat" placeholder="Latitude"/>
+            <input type="text" name="lng" placeholder="Longitude"/>
+            <input type="submit" value="Get TTF"/>
+        </form>
+    ''' ##MÅ OPPDATERE ENDPOINT NÅR VI HAR FÅTT API
 
 
 if __name__ == '__main__':

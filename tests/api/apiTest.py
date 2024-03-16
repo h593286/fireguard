a0acd14dc0a5c2ccc12b6600b5457496c25c4d3b
MaggeSN <122047522+MaggeSN@users.noreply.github.com>
2024-03-16 19:45:08 +0100

    add tests api

--- tests/api/apiTest.py
+++ tests/api/apiTest.py
@@ -2,11 +2,17 @@ from unittest import TestCase, main
 from unittest.mock import MagicMock
 from src.api.requesthandler.api_logic import FireLogic
 from src.api.requesthandler.buildFireguardAPI import load_cities
-from src.service.frcapi import FireRiskModelAPI
+from src.service.frcapi import FireRiskModelAPI, FireRiskPrediction,Location,FireRisk
+import datetime
+
 
 class MyTestCase(TestCase):
     def setUp(self):
-        cities = load_cities()
+        city = "Oslo"
+        latitiude = 59.9133
+        longitude = 10.7389
+        
+        cities = [{"city": city, "lat": latitiude, "lng": longitude}]
         self.firelogic = FireLogic(name="Fireguard", cities=cities, modelApi=MagicMock(spec=FireRiskModelAPI))
 
     def test_read_city(self):
@@ -44,7 +50,40 @@ class MyTestCase(TestCase):
         else:
             self.fail(f"read_city_by_coordinates({lat}, {lng}) returned a city string, expected None")
 
+    def test_get_firerisk_by_city(self):
+            # Arrange
+            city = "Oslo"
+            latitude = 59.9133
+            longitude = 10.7389
+            expected_prediction = FireRiskPrediction(location=Location(latitude=latitude, longitude=longitude), firerisks=[FireRisk(timestamp=datetime.datetime.now(), ttf=1.5)])
+            self.firelogic.modelApi.compute = MagicMock(return_value=expected_prediction)
 
+            # Act
+            result = self.firelogic.get_firerisk_by_city(city)
+
+            # Assert
+            self.firelogic.modelApi.compute.assert_called_once_with(Location(latitude=latitude, longitude=longitude))
+            self.assertEqual(result, expected_prediction)
+
+    def test_get_firerisk_by_coordinates(self):
+        # Arrange
+        latitude = 59.9133
+        longitude = 10.7389
+        expected_prediction = FireRiskPrediction(location=Location(latitude=latitude, longitude=longitude), firerisks=[FireRisk(timestamp=datetime.datetime.now(), ttf=1.5)])
+        self.firelogic.modelApi.compute = MagicMock(return_value=expected_prediction)
+
+        # Act
+        result = self.firelogic.get_firerisk_by_coordinates(latitude, longitude)
+
+        # Assert
+        self.firelogic.modelApi.compute.assert_called_once_with(Location(latitude=latitude, longitude=longitude))
+        self.assertEqual(result, expected_prediction)
+
+
+    #Intergration tests
+    def test_get_cities(self):
+        cities = load_cities()
+        pass
 
 
 if __name__ == '__main__':

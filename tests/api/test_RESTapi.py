7527c85400a17e415c266dbbd9a6bebb2ec4fc17
ReddikHaien <584969@stud.hvl.no>
2024-04-19 00:36:16 +0200

    tests are alive

--- tests/api/test_RESTapi.py
+++ tests/api/test_RESTapi.py
@@ -10,8 +10,9 @@ import urllib.parse
 
 # Fireguard API
 from src.api.requesthandler.api_logic import FireLogic
+from src.data.dataTypes import FireRisk
 from src.api.requesthandler.buildFireguardAPI import load_cities
-from src.service.frcapi import FireRiskModelAPI, FireRiskPrediction,Location,FireRisk
+from src.service.frcapi import FireRiskModelAPI, FireRiskPrediction,Location
 import src.api.requesthandler.api_server as api_server
 
 

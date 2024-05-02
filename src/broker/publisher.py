066ad32134f18f97ca6c3fa430fb712499a6f66a
h593286 <593286@stud.hvl.no>
2024-05-02 11:08:06 +0200

    Update publisher.py

--- src/broker/publisher.py
+++ src/broker/publisher.py
@@ -1,17 +1,26 @@
 import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-brokerURL = 'c406645d204a4c93919e442f4c8bcc09.s1.eu.hivemq.cloud'
-port = 8883
+class Publisher:
+    def __init__(self):
+        self.brokerURL = 'c406645d204a4c93919e442f4c8bcc09.s1.eu.hivemq.cloud'
+        self.port = 8883
+        self.client = mqtt.Client()
+        self.client.username_pw_set(os.getenv('PUBLISHER_USERNAME'),os.getenv('PUBLISHER_PASSWORD'))
+        self.client.tls_set()  # Set up TLS
 
-client = mqtt.Client()
-client.tls_set()  # Set up TLS
+    ##publish
+    def connect(self):
+        self.client.connect(self.brokerURL, self.port, 60)
+    def disconnect(self):
+        self.client.disconnect()
 
-##publish
-def connect():
-    client.connect("brokerURL", port, 60)
-def disconnect():
-    client.disconnect()
+    def publish(self,city, TTF):
+        self.client.publish("test/topic", city + ", TTF: " + TTF)
 
-def publish(city, TTF):
-    client.publish("test/topic", city + ", TTF: " + TTF)
+        #Correct when implementation tested works
+        #self.client.publish("city/"+city, TTF)
+
